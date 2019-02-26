7d02dec80ca12615b6fe77b148f228358b162330
Kamal Mukherjee <kamalnyc17@gmail.com>
2019-02-26 13:47:09 -0500

    Train Schedule

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,29 @@ var config = {
     projectId: "train-time-table-34db9",
     storageBucket: "train-time-table-34db9.appspot.com",
     messagingSenderId: "600414114017"
-  };
-  firebase.initializeApp(config);
-  
-  var dataRef = firebase.database();
+};
+firebase.initializeApp(config);
 
-  // variables for the schedule table
-  
+var dataRef = firebase.database();
+
+// variables for the schedule table
+var trainName = "";
+var destination = "";
+var trainTime = "00:00";
+var minutesAway = "";
+var trainFrequency = "1";
+var nextArrival = "00:00";
+
+// actions after clicking on submit button
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    // grabing field values from browser
+
+    trainName = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    trainTime = $("#start-input").val().trim();
+    trainFrequency = $("#frequency-input").val().trim();
+    minutesAway = "1";
+    nextArrival = "00:00";
+});
